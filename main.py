6a89403102163ca3ff3394b6856e40e5983a91db
Noman Ahmed <64908635+nomanahmedna@users.noreply.github.com>
2022-04-26 14:36:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
-import requests
+import requests, csv
 from bs4 import BeautifulSoup
 
-companies = ['PAKRI','POL','PPL','SYS','UBL','GTECH','OGDC','MTL','MCB','HUBC','HINOON','EPCL','EFERT','DAWH','AVN']
+companies = ['PAKRI','PPL','GTECH','OGDC','HUBC','EPCL','EFERT']
 
-page = requests.get("https://dps.psx.com.pk/company/POL")
-soup = BeautifulSoup(page.content, 'html.parser')
+for i in companies:
+    print(i)
 
-share_price = soup.find_all('div', class_='stats_item')[0:3]
-current_price = soup.find_all('div', class_='quote__close')
+    page = requests.get("https://dps.psx.com.pk/company/" + i)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    share_price = soup.find_all('div', class_='stats_item')[0:3]
+    current_price = soup.find_all('div', class_='quote__close')
+    share_name = soup.find_all('div', class_='quote__name')
 
 
-for value in share_price:
-    print(value.text)
+    for value2 in share_name:
+        print(value2.text)
 
-for value1 in current_price:
-    print(value1.text)
+    for value in share_price:
+            print(value.text)
+
+    for value1 in current_price:
+            print(value1.text)
+
